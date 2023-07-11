93ef6e65f0f67daed93a53d4df69dca60c6639fd
Nathan <dzenathan1998@gmail.com>
2023-07-11 20:28:51 +0200

    update-endpiont corrected

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -5,7 +5,6 @@ const axiosInstance = axios.create();
 axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
 
-// Add a request interceptor
 axiosInstance.interceptors.request.use(
   (config) => {
    
